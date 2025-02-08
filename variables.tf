706e6b3d27b22272f4008e09f44150ae34eb15e4
pritijadhao <pritijadhao62@gmail.com>
2025-02-08 21:13:35 +0530

    variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,21 @@
+variable "my_region" {
+  type = string
+}
 
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "my_ami" {
+  type = string
+}
+variable "vpc_id" {
+  type = string
+}
+variable "instance_type" {
+  type = string
+}
